59d0a022dfb6659b2f46e8fa271d0188b684bc9b
Nithesh Narayanan <nithuan141@gmail.com>
2022-07-10 13:07:01 +0530

    chore: added proper comments

--- dpk.js
+++ dpk.js
@@ -1,6 +1,11 @@
 const crypto = require("crypto");
 const TRIVIAL_PARTITION_KEY = "0";
 
+/**
+ * Accepts the event input and return the corresponding candidate partition key.
+ * @param {Object} event 
+ * @returns 
+ */
 exports.deterministicPartitionKey = (event) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate = getCandidateFromEvent(event);
@@ -12,10 +17,20 @@ exports.deterministicPartitionKey = (event) => {
   return candidate;
 };
 
+/**
+ * Returns the crypto hash for the given string input.
+ * @param {String} data 
+ * @returns 
+ */
 const getCryptoHash = (data) => {
   return crypto.createHash("sha3-512").update(data).digest("hex")
 }
 
+/**
+ * Accepts the event object and returns the candidate key.
+ * @param {Object} event 
+ * @returns 
+ */
 const getCandidateFromEvent = (event) => {
   if(event) {
     return JSON.stringify(event?.partitionKey) || getCryptoHash(JSON.stringify(event))
